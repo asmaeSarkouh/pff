593aecfb2366f6bd88beff712e7f4035c3dd90d4
asmaeSarkouh <106113813+asmaeSarkouh@users.noreply.github.com>
2023-05-14 23:24:35 +0200

    Update EspaceMaintenanceFactory.php

--- database/factories/EspaceMaintenanceFactory.php
+++ database/factories/EspaceMaintenanceFactory.php
@@ -14,7 +14,12 @@ class EspaceMaintenanceFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'type_maintenance_id'=>TypeMaintenance::inRandomOrder()->first(),
+            'date'=>$this->faker->date(),
+            'kilometrage'=>$this->faker->randomDigit(100),
+            'frais'=>$this->faker->randomDigit(1000),
+            'image'=>$this->faker->sentence(2),
+            'espace_vehicule_id'=>EspaceVehicule::inRandomOrder()->first(),
         ];
     }
 }
