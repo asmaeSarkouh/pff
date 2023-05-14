0cf11892c3b7ff4642245caecd800afebcc09323
asmaeSarkouh <106113813+asmaeSarkouh@users.noreply.github.com>
2023-05-14 21:47:34 +0200

    Add files via upload

---
+++ bootstrap/cache/livewire-components.php
@@ -0,0 +1,8 @@
+<?php return array (
+  'auth.login' => 'App\\Http\\Livewire\\Auth\\Login',
+  'auth.passwords.confirm' => 'App\\Http\\Livewire\\Auth\\Passwords\\Confirm',
+  'auth.passwords.email' => 'App\\Http\\Livewire\\Auth\\Passwords\\Email',
+  'auth.passwords.reset' => 'App\\Http\\Livewire\\Auth\\Passwords\\Reset',
+  'auth.register' => 'App\\Http\\Livewire\\Auth\\Register',
+  'auth.verify' => 'App\\Http\\Livewire\\Auth\\Verify',
+);
