d701b86993bd74d0a07c956f00a4437898cdbc2c
asmaeSarkouh <106113813+asmaeSarkouh@users.noreply.github.com>
2023-05-14 23:25:13 +0200

    Update EspaceVehiculeFactory.php

--- database/factories/EspaceVehiculeFactory.php
+++ database/factories/EspaceVehiculeFactory.php
@@ -14,7 +14,17 @@ class EspaceVehiculeFactory extends Factory
     public function definition()
     {
         return [
-            //
+            
+           'nom_vehicule'=>$this->faker->word(),
+            'immatricule'=>$this->faker->sentence(2),
+           'type_carburant_id'=>TypeCarburant::inRandomOrder()->first(),
+            'consomation'=>$this->faker->randomDigit(100),
+            'marque_id'=>Marque::inRandomOrder()->first(),
+            'modele_id'=>Modele::inRandomOrder()->first(),
+            'annee_mise_id'=>AnneeMise::inRandomOrder()->first(),
+            'kilometrage'=>$this->faker->randomDigit(100),
+            'image'=>$this->faker->sentence(2),
+            'user_id'=>User::inRandomOrder()->first()
         ];
     }
 }
